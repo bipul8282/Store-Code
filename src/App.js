6fc98eec5c875f7fd388b9f86e6f42c0ee93740c
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2023-11-16 22:15:42 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 import "./App.css";
 import {Link} from "react-router-dom"
 import NavBar from  "./NavBar"
+import Search from "./Search";
+import Image from "./Image";
+import Guide from "./Guide";
+import StoreCode from "./StoreCode";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div>
          <NavBar/>
-      <h1>Home</h1>
-       
+      <Search/>
+      <Image/>
+       <Guide/>
+       <StoreCode/>
+       <Footer/>
     </div>
   );
 }
